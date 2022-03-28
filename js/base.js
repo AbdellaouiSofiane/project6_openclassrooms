450282e4de2ffd7dd481244ffdfbc206cb6b5e70
sofiane <abdellaoui.sofiane.esb@gmail.com>
2022-03-28 14:23:27 +0200

    add modal on image click

--- js/base.js
+++ js/base.js
@@ -1,3 +1,5 @@
+(function() {
+
 const base_url = "http://localhost:8000/api/v1/titles/?"
 
 const category_1 = "Action"
@@ -6,29 +8,34 @@ const category_3 = "Comedy"
 const category_list = [category_1, category_2, category_3]
 
 const max_movies_by_category = 7
-
 const max_visible_by_category = 4
+
+const modal = document.getElementById("myModal");
+var close = document.getElementsByClassName("close")[0];
 
 
 function append_movie_node(parent, key, index) {
 	const movie_element = document.createElement("li");
-	const movie_link = document.createElement("a");
-	const movie_img = document.createElement("img");
-
 	movie_element.setAttribute("class", "slide");
-	movie_link.setAttribute("href", key.imdb_url);
-	movie_img.setAttribute("src", key.image_url);
-	movie_img.setAttribute("alt", key.title);
-
 	if (index < max_visible_by_category) {
 		movie_element.classList.add('visible')
 	}
 
+	const movie_link = document.createElement("a");
+	movie_link.setAttribute("data_url", key.url);
+	movie_link.setAttribute("class", "btn");
+
+	const movie_img = document.createElement("img");
+	movie_img.setAttribute("src", key.image_url);
+	movie_img.setAttribute("alt", key.title);
+
 	movie_link.appendChild(movie_img);
 	movie_element.appendChild(movie_link);
-	parent.appendChild(movie_element)
+	parent.appendChild(movie_element);
 
-
+	movie_link.addEventListener('click', function() {
+		display_modal(key.url)
+	})
 };
 
 
@@ -79,3 +86,37 @@ for (category of category_list) {
 	custom_fetch(base_url, {genre: category, sort_by: "-imdb_score"}, node_list)
 	i++
 }
+
+// hide modal when user clicks the close button
+close.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Hide modal when user clicks the close button anywhere outside of the modal
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function display_modal(url) {
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			document.getElementById('modal-img').setAttribute("src", data.image_url);
+			document.getElementById('modal-title').innerHTML = data.title;
+			document.getElementById('modal-description').innerHTML = data.long_description;;
+			document.getElementById('modal-date-published').innerHTML = data.date_published;
+			document.getElementById('modal-avg-vote').innerHTML = data.avg_vote;
+			document.getElementById('modal-imdb').innerHTML = data.imdb_score;
+			document.getElementById('modal-duration').innerHTML = data.duration;
+			document.getElementById('modal-directors').innerHTML = data.directors.join(', ');
+			document.getElementById('modal-countries').innerHTML = data.countries.join(', ');
+			document.getElementById('modal-actors').innerHTML = data.actors.join(', ');
+		});
+
+	modal.style.display = "block";
+}
+
+
+})()
